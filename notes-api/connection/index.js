fa87fea460b3c89868ea8551afb1b0696bf38210
Ivan Alves <x.surian.x@gmail.com>
2016-07-29 21:40:50 -0300

    ultima aula angular js

--- notes-api/connection/index.js
+++ notes-api/connection/index.js
@@ -2,22 +2,13 @@
  * Created by atilla8huno on 08/07/16.
  */
 var mongoose = require('mongoose');
-var mongodb = require('./mongodb');
-
-const URI = '' +
-    mongodb.serverName + '://' +
-    mongodb.username + ':' +
-    mongodb.password + '@' +
-    mongodb.dns + ':' +
-    mongodb.port + '/' +
-    mongodb.database;
 
 var connection = {
     connect: function () {
-        mongoose.connect(URI, function (err) {
+        mongoose.connect('mongodb://admin:admin@ds031965.mlab.com:31965/mynotesweb', function (err) {
             if (err) throw err;
 
-            console.log('MongoDB connected at ' + URI);
+            console.log('MongoDB connected mongo');
         });
     }
 };
--- notes-api/services/firebase.service.js
+++ notes-api/services/firebase.service.js
@@ -2,13 +2,12 @@
  * Created by atilla8huno on 15/07/2016.
  */
 var firebase = require('firebase');
-var Promise = require('promise');
 
 // Firebase setup
 firebase.initializeApp({
     serviceAccount: "./connection/firebase.json",
-    authDomain: "mean-course-2.firebaseapp.com",
-    databaseURL: "https://mean-course-2.firebaseio.com"
+    authDomain: "notes-web-atilla-439a7.firebaseapp.com",
+    databaseURL: "https://notes-web-atilla-439a7.firebaseio.com"
 });
 
 var auth = firebase.auth();
